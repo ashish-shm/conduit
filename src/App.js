74be4e495f7c932867b4ff6c5b40ba5807be712c
Ashish Sharma <ashish.shm09@gmail.com>
2020-10-08 13:20:44 +0530

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import LoginPage from "./components/LoginPage";
+import Register from "./components/Register";
+import DashBoard from "./components/DashBoard";
 
 function App() {
   return (
-    <div>
-      <h1>Conduit</h1>
-    </div>
+    <main>
+      <Switch>
+        <Route path="/" component={HomePage} exact />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={Register} exact />
+        <Route path="/dasshboard" component={DashBoard} exact />
+      </Switch>
+    </main>
   );
 }
 
